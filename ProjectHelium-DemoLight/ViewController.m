8455f06b68761ccff56db2260421f908b63b0cf8
BernardYan <ybn@me.com>
2016-06-24 20:18:09 +0800

    now it works as a demo light

--- ProjectHelium-DemoLight/ViewController.m
+++ ProjectHelium-DemoLight/ViewController.m
@@ -7,8 +7,16 @@
 //
 
 #import "ViewController.h"
+@import CoreBluetooth;
+@import CoreLocation;
 
-@interface ViewController ()
+@interface ViewController ()<CBPeripheralManagerDelegate, CLLocationManagerDelegate>
+
+@property (strong, nonatomic) CBPeripheralManager *peripheralManager;
+@property (strong, nonatomic) CLLocationManager *locationManager;
+@property (strong, nonatomic) CLBeaconRegion *broadcastRegion;
+@property (strong, nonatomic) CLBeaconRegion *rangingRegion;
+@property (strong, nonatomic) NSMutableDictionary *peripheralData;
 
 @end
 
@@ -17,7 +25,65 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"View did load");
+    
+    self.view.backgroundColor = [UIColor darkGrayColor];
+    
+    self.peripheralManager = [[CBPeripheralManager alloc] initWithDelegate:self
+                                                                     queue:nil
+                                                                   options:nil];
+    
+    NSUUID *broadcastUUID = [[NSUUID alloc] initWithUUIDString:@"778DCA1F-1817-4D78-BD4F-F7549155D914"];
+    NSUUID *rangingUUID = [[NSUUID alloc] initWithUUIDString:@"778DCA1F-1817-4D78-BD4F-F7549155D915"];
+    
+    self.broadcastRegion = [[CLBeaconRegion alloc] initWithProximityUUID:broadcastUUID major:0 minor:0 identifier:@"Helium-broadcast"];
+    self.rangingRegion = [[CLBeaconRegion alloc] initWithProximityUUID:rangingUUID identifier:@"Helium-receive"];
+    
+    
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.delegate = self;
+    
+    if([self.locationManager respondsToSelector:@selector(requestAlwaysAuthorization)]) {
+        [self.locationManager requestAlwaysAuthorization];
+    }
+    
+    [self.locationManager startRangingBeaconsInRegion:self.rangingRegion];
+    
 }
+
+- (void)peripheralManagerDidUpdateState:(CBPeripheralManager *)peripheral {
+    
+    if (peripheral.state == CBPeripheralManagerStatePoweredOn) {
+        
+        self.peripheralData = [self.broadcastRegion peripheralDataWithMeasuredPower:nil];
+        [self.peripheralManager startAdvertising:self.peripheralData];
+        
+    }
+    
+}
+
+-(void)locationManager:(CLLocationManager *)manager didRangeBeacons:(NSArray<CLBeacon *> *)beacons inRegion:(CLBeaconRegion *)region {
+    
+    CLBeacon *beacon = [beacons firstObject];
+
+    if ([beacon.major isEqual:[NSNumber numberWithInt:11]]) {
+        
+        if (beacon.rssi == 0) {//when beacon broadcast stops, the delegate still get called for a few seconds, but we can know it from rssi value
+            self.view.backgroundColor = [UIColor darkGrayColor];
+        }else{
+            self.view.backgroundColor = [UIColor whiteColor];
+        }
+        
+    }else{
+        self.view.backgroundColor = [UIColor darkGrayColor];
+    }
+    
+    //NSLog([beacon.major description]);
+    NSLog([beacon description]);
+    
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
