fe6243ca7afc4e6af01ea5a1e4fe6b9478a31452
BernardYan <ybn@me.com>
2016-06-24 11:34:47 +0800

    Initial Commit

---
+++ ProjectHelium-DemoLight/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProjectHelium-DemoLight
+//
+//  Created by Bernard Yan on 6/24/16.
+//  Copyright © 2016 IntBridge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
